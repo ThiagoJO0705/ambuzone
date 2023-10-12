3d09c488c185c1485d7f291f8598e9f7629657c6
santos-nikolas <rm551566@fiap.com.br>
2023-10-12 17:29:15 -0300

    iniciando cabecalho

--- src/Components/Cabecalho.jsx
+++ src/Components/Cabecalho.jsx
@@ -2,10 +2,15 @@ import './Cabecalho.css'
 
 export default function Cabecalho(){
     return(
-        <>
         <header>
-            
+            <nav>
+                <h1>Ambuzone</h1>
+            </nav>
+            <div>
+                <p>Home</p>
+                <p>Dados Maquete</p>
+                <p>Geração de rotas</p>
+            </div>
         </header>
-        </>
     )
 }
