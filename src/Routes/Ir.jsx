9ae725ddd037d24a5fbccf56b4edc3502c9bee29
santos-nikolas <rm551566@fiap.com.br>
2023-10-18 20:14:48 -0300

    iniciando rota Ir

--- src/Routes/Ir.jsx
+++ src/Routes/Ir.jsx
@@ -1,8 +1,14 @@
+export default function Ir() {
+    console.log('renderizando Ir');
+    // const iframeStyle = {
+    //     border: '0',
+    //     width: 600,
+    //     height: 450,
 
+    // };
 
-export default function Ir(){
-    console.log('renderizando Ir')
-    return(
-        <h1>Ir</h1>
-    )
+    return (
+        // <iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d117007.64216179494!2d-46.773289859513035!3d-23.586795980050603!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e0!4m5!1s0x94cef8a6b0e6b435%3A0x6539c61690d9cf06!2sAv.%20Manuel%20Bandeira%2C%20360%20-%20Vila%20Leopoldina%20S%C3%A3o%20Paulo%20-%20SP%2005317-020!3m2!1d-23.5410242!2d-46.7340229!4m5!1s0x94ce5a264d6b6611%3A0xae18ff6a2797997b!2sHospital%20S%C3%A3o%20Paulo%20-%20Rua%20Napole%C3%A3o%20de%20Barros%20-%20Vila%20Clementino%2C%20S%C3%A3o%20Paulo%20-%20SP!3m2!1d-23.5974526!2d-46.643741899999995!5e0!3m2!1spt-BR!2sbr!4v1697670116087!5m2!1spt-BR!2sbr" style={iframeStyle} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <></>
+    );
 }
