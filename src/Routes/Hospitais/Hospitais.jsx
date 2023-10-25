514431f17b8ea7e93a78693e0a72569ef6302b67
santos-nikolas <rm551566@fiap.com.br>
2023-10-25 12:05:05 -0300

    finalizando grande parte da aba hospitais

---
+++ src/Routes/Hospitais/Hospitais.jsx
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from "react";
+import {FaLocationDot as LocalizacaoIcon} from 'react-icons/fa6'
+import haversine from 'haversine-distance';
+import styles from './Hospitais.module.css'
+
+export default function Hospitais() {
+const [userLocation, setUserLocation] = useState(null);
+useEffect(() => {
+  // Função para obter a localização do usuário
+  const getUserLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const userCoords = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+
+        setUserLocation(userCoords);
+      });
+    }
+  };
+
+  getUserLocation();
+}, []);  
+  const [hospitais, setHospitais] = useState([]);
+
+  useEffect(() => {
+    if (!userLocation) {
+      return;
+    }
+
+    const script = document.createElement("script");
+    script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAsBhXcnErJepuDKf6qyO3yAyToVXsxXxc&libraries=places`;
+    script.async = true;
+    script.defer = true;
+
+    script.onload = () => {
+      const service = new window.google.maps.places.PlacesService(document.createElement("div"));
+      service.nearbySearch(
+        {
+          location: userLocation,
+          radius: 1000, //raio em metros para busca
+          type: "hospital", // Tipo de lugar a ser pesquisado
+        },
+        (results, status) => {
+          if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+            const hospitalsWithDistance = results.map((hospital) => {
+              const hospitalCoords = {
+                lat: hospital.geometry.location.lat(),
+                lng: hospital.geometry.location.lng(),
+              };
+              const distance = haversine(userLocation, hospitalCoords);
+
+              // Condicionar a unidade de medida
+              const formattedDistance = distance+300 >= 1000 ? `${((distance+300) / 1000).toFixed(2)} km` : `${Math.round(distance+300)} m`;
+      
+              // Adicione a distância ao objeto do hospital
+              return {
+                ...hospital,
+                distance: formattedDistance,
+              };
+            });
+      
+            setHospitais(hospitalsWithDistance);
+            console.log(hospitalsWithDistance);
+            console.log('Hospitais capturados');
+          } else {
+            console.error('Erro ao buscar hospitais:', status);
+          }
+        }
+      );
+    };
+
+    document.head.appendChild(script);
+  }, [userLocation]);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.cabecalho}>
+      <div className={styles.titulo}>
+      <p><LocalizacaoIcon/> São Paulo</p>
+      <h1>Hospitais Próximos</h1>
+      </div>
+      <div className={styles.raioDeBusca}></div>
+      </div>
+      <ul>
+        {hospitais.map((hospital) => (
+          <li key={hospital.place_id}>
+            <strong>{hospital.name}</strong>
+            <p>{hospital.vicinity}</p>
+            <p>{hospital.distance}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
