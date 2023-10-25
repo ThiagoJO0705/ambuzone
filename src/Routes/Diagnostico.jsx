eff29e40845d9166c991a5a6ba2002ca66a4016d
ThiagoJO0705 <rm551624@fiap.com.br>
2023-10-25 14:54:31 -0300

    classificação de risco pronta

--- src/Routes/Diagnostico.jsx
+++ src/Routes/Diagnostico.jsx
@@ -3,7 +3,9 @@ import style from './Diagnostico.module.css'
 export default function Diagnostico(){
 
       function handleSubmit(event) {
-        event.preventDefault();
+        event.preventDefault(); 
+
+        alert("Preencha todos os campos!")
       }
 
     return(
@@ -14,43 +16,43 @@ export default function Diagnostico(){
                     <form onSubmit={handleSubmit} className={style.form}>
                         <div className={style.dados}>
                             <label htmlFor="dados">Dados do Paciente:</label>
-                            <input className={style.dadosinput} type="text" id='dados' name='dados' placeholder='Digie os Dados do paciente...'/>
+                            <input className={style.dadosinput} type="text" id='dados' name='dados' placeholder='Digie os Dados do paciente...' value="" />
                         </div>
 
                         <div className={style.sintomas}>
                             <label htmlFor="sintomas">Sintomas:</label>
-                            <textarea className={style.sintomasinput} id='sintomas' name='sintomas' placeholder='Digite os Sintomas do paciente...' cols="20" rows="5" />
+                            <textarea className={style.sintomasinput} id='sintomas' name='sintomas' placeholder='Digite os Sintomas do paciente...' cols="20" rows="5" value="" />
                         </div>
 
                         <div className={style.procedimento}>
                             <label htmlFor="procedimento">Procedimento:</label>
-                            <textarea className={style.procedimentoinput} id='procedimento' name='procedimento' placeholder='Digite o Pocedimento realizado...'/>
+                            <textarea className={style.procedimentoinput} id='procedimento' name='procedimento' placeholder='Digite o Pocedimento realizado...' value="" />
                         </div>
 
                         <div className={style.obs}>
                             <label htmlFor="obs">Observações:</label>
-                            <textarea className={style.obsinput}  id='obs' name='obs' placeholder='Digite as Observações...'/>
+                            <textarea className={style.obsinput}  id='obs' name='obs' placeholder='Digite as Observações...' value="" />
                         </div>
 
                         <div className={style.risco}>
                             <span> Classificação de Risco:</span>
                             <input className={style.naourgente} type="radio" id='naourgente' name='risco' value="Não Urgente"/>
-                            <label for="naourgente"></label>
+                            <label for="naourgente">1</label>
 
                             <input className={style.poucourgente} type="radio" id='poucourgente' name='risco' value="Pouco Urgente"/>
-                            <label for="poucourgente"></label>
+                            <label for="poucourgente">2</label>
 
                             <input className={style.urgente} type="radio" id='urgente' name='risco' value="Urgente"/>
-                            <label for="urgente"></label>
+                            <label for="urgente">3</label>
 
                             <input className={style.muitourgente} type="radio" id='muitourgente' name='risco' value="Muito Urgente"/>
-                            <label for="muitourgente"></label>
+                            <label for="muitourgente">4</label>
 
                             <input className={style.emergencia} type="radio" id='emergencia' name='risco' value="Emergência"/>
-                            <label for="emergencia"></label>
+                            <label for="emergencia">5</label>
                         </div>
 
-                        <input type="submit" />
+                        <button type='submit'>Enviar</button>
                     </form>
                 </div>
             </section>
