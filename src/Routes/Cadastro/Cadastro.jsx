91265235e85470787ff86d32a86b4f39267b83ac
ThiagoJO0705 <rm551624@fiap.com.br>
2023-11-09 14:10:44 -0300

    validacao em andamento

--- src/Routes/Cadastro/Cadastro.jsx
+++ src/Routes/Cadastro/Cadastro.jsx
@@ -9,6 +9,7 @@ export default function Cadastro(){
     const [spanErroUsuarioVisible, setSpanErroUsuarioVisible] = useState(false);
     const [spanErroEmailVisible, setSpanErroEmailVisible] = useState(false);
     const [spanErroSenhaVisible, setSpanErroSenhaVisible] = useState(false);
+    const [spanErroSenhaNumeroVisible, setSpanErroSenhaNumeroVisible] = useState(false)
 
 
     // Adicionando o id automaticamente
@@ -29,45 +30,100 @@ export default function Cadastro(){
         senha: ""
     })
 
+    
     const handleChange = (e)=>{
         const emailRegex = new RegExp(
             // Exemplo de email valido -> usuario123@host.com.br
             /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z{2,}$]/
         );
         //Destructuring
-        const {name,value} = e.target;
       //Setando os dados diretamente no objeto atravé de SPREAD
+        
+
+        // if (name === "nome" && cadastro.nome.length < 3){
+        //     // while (cadastro.nome.length < 3){
+        //         setSpanErroNomeVisible(true);
+        //     // }
+        //     console.log(cadastro)
+        // } else {
+            //     setSpanErroNomeVisible(false)
+            // }
+            
+            // if (name === 'usuario' && value.length < 3){
+                //     setSpanErroUsuarioVisible(true);
+                //     console.log(cadastro)
+                // } else {
+                    //     setSpanErroUsuarioVisible(false)
+                    // } 
+                    // if (emailRegex.test(cadastro.email)){
+                        //     setSpanErroEmailVisible(true);
+                        //     console.log(cadastro)
+                        // } else {
+                            //     setSpanErroEmailVisible(false)
+                            // }
+                            
+                            // if (name === 'senha' && value.length < 5){
+                                //     setSpanErroSenhaVisible(true);
+                                //     console.log(cadastro)
+                                // } 
+                                // else if (!(senha in"0123456789" )) {
+                                    //     setSpanErroSenhaNumeroVisible(true)
+                                    // }
+                                    // else {
+                                        //     setSpanErroSenhaVisible(false)
+        //     setSpanErroSenhaNumeroVisible(false)
+        // }
+        
+    }
+    
+    
+    
+    
+    const handleName = (e)=>{
+        const {name,value} = e.target;
         setCadastro({...cadastro,[name]:value});
 
-        if (name === "nome" && cadastro.nome.length < 3){
-            setSpanErroNomeVisible(true);
-            console.log(cadastro)
-        } else {
+        if (cadastro.nome.length < 2){
+            setSpanErroNomeVisible(true)
+            console.log(cadastro.nome)
+        }
+        else {
             setSpanErroNomeVisible(false)
         }
-        
-        if (name === 'usuario' && value.length < 3){
-            setSpanErroUsuarioVisible(true);
-            console.log(cadastro)
-        } else {
-            setSpanErroUsuarioVisible(false)
-        } 
-        if (emailRegex.test(cadastro.email)){
-            setSpanErroEmailVisible(true);
-            console.log(cadastro)
-        } else {
-            setSpanErroEmailVisible(false)
-        }
-        
-        if (name === 'senha' && value.length < 5){
-            setSpanErroSenhaVisible(true);
-            console.log(cadastro)
-        } else {
-            setSpanErroSenhaVisible(false)
-        }
-        
-        }
+    }
+    
 
+      const handleUser = (e)=>{
+        const {name,value} = e.target;
+        setCadastro({...cadastro,[name]:value});
+
+        if (cadastro.usuario.length < 2){
+            setSpanErroUsuarioVisible(true)
+            console.log(cadastro.usuario)
+        }
+        else {
+            setSpanErroUsuarioVisible(false)
+        }
+      }
+
+
+      const handlePassword = (e)=>{
+        const {name,value} = e.target;
+        setCadastro({...cadastro,[name]:value});
+
+        if (cadastro.senha.length < 5){
+            setSpanErroSenhaVisible(true)
+            console.log(cadastro.senha)
+        }
+        else if (!/\d/.test(cadastro.senha)){
+            setSpanErroSenhaNumeroVisible(true)
+        }
+        else {
+            setSpanErroSenhaVisible(false)
+            setSpanErroSenhaNumeroVisible(false)
+        }
+      }
+    
 
       const handleSubmit = (e) =>{
         e.preventDefault();
@@ -99,13 +155,13 @@ export default function Cadastro(){
                 <form className="form" onSubmit={handleSubmit}>
                     <div className="nome">
                         <label htmlFor="nome">Nome</label>
-                        <input type="text" name="nome" placeholder="Digite seu nome completo" value={cadastro.nome} onChange={handleChange} />
+                        <input type="text" name="nome" placeholder="Digite seu nome completo" value={cadastro.nome} onChange={handleName} />
                         <span className={spanErroNomeVisible ? "errospan" : "spanescondido"}>Mínimo de 3 caracteres !</span>
                     </div>
 
                     <div className="usuario">
                         <label htmlFor="usuario">Usuário</label>
-                        <input type="text" name="usuario" placeholder="Digite um nome de usuário" value={cadastro.usuario} onChange={handleChange} />
+                        <input type="text" name="usuario" placeholder="Digite um nome de usuário" value={cadastro.usuario} onChange={handleUser} />
                         <span className={spanErroUsuarioVisible ? "errospan" : "spanescondido"}>Mínimo de 3 caracteres !</span>
                     </div>
 
@@ -117,8 +173,9 @@ export default function Cadastro(){
 
                     <div className="senha">
                         <label htmlFor="senha">Senha</label>
-                        <input type="password" name="senha" placeholder="Digite sua senha" value={cadastro.senha} onChange={handleChange} />
+                        <input type="password" name="senha" placeholder="Digite sua senha" value={cadastro.senha} onChange={handlePassword} />
                         <span className={spanErroSenhaVisible ? "errospan" : "spanescondido"}>Mínimo de 5 caracteres !</span>
+                        <span className={spanErroSenhaNumeroVisible ? "errospan" : "spanescondido"}>Precisa conter 1 número no mínimo</span>
                     </div>
 
                     <button type="submit" className="entrar">Cadastrar</button>
