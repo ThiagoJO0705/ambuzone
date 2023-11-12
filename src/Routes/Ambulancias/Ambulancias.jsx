0a0bb86b7b3e8d7402b519ffcf9d0119a22db81f
Rodrigo-Brasileiro <rm98952@fiap.com.br>
2023-11-12 17:22:08 -0300

    Ambulancia sem estilização

--- src/Routes/Ambulancias/Ambulancias.jsx
+++ src/Routes/Ambulancias/Ambulancias.jsx
@@ -1,35 +1,38 @@
-import style from "./Ambulancias.module.css"
 import "../../../public/img/ambuzone.png"
-
+ 
 export default function Ambulancias(){
-  
+ 
   document.title = 'Ambulâncias'
-  
-  return(
-    <>
-      <div className={style.container}>
-        <div className={style.boxazul}>
-          <img src="../../../public/img/ambuzone.png" alt="" />
+  if(sessionStorage.getItem("token-user") || localStorage.getItem("token-user")){
+ 
+    return(
+      <>
+        <div className="container">
+          <div className="boxazul">
+            <img src="../../../public/img/ambuzone.png" alt="" />
+          </div>
+          <div className="boxbranca">
+            <div className="ambativas">
+              <h1>Ambulâncias Ativas:</h1>
+              <p>672 Ambulâncias.</p>
+            </div>
+            <div className="ambdisponiveis">
+              <h1>Ambulâncias Disponíveis:</h1>
+              <p>388 Ambulâncias.</p>
+            </div>
+            <div className="ambocorrencias">
+              <h1>Em Ocorrência:</h1>
+              <p>284 Ambulâncias.</p>
+            </div>
+            <div className="ocorrencias">
+              <h1>Ocorrências do Dia:</h1>
+              <p>427 Ocorrências.</p>
+            </div>
+          </div>
         </div>
-        <div className={style.boxbranca}>
-          <div className={style.ambativas}>
-            <h1>Ambulâncias Ativas:</h1>
-            <p>672 Ambulâncias.</p>
-          </div>
-          <div className={style.ambdisponiveis}>
-            <h1>Ambulâncias Disponíveis:</h1>
-            <p>388 Ambulâncias.</p>
-          </div>
-          <div className={style.ambocorrencias}>
-            <h1>Em Ocorrência:</h1>
-            <p>284 Ambulâncias.</p>
-          </div>
-          <div className={style.ocorrencias}>
-            <h1>Ocorrências do Dia:</h1>
-            <p>427 Ocorrências.</p>
-          </div>
-        </div>
-      </div>
-    </>
-  )
+      </>
+    )
+  } else {
+    window.location = "/login"
+  }
 }
